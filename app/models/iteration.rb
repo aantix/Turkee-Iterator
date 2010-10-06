69d59535370d13319af4bfc80e1c7b361bdfdf22
Jim Jones <jjones@aantix.com>
2010-10-06 11:32:51 -0700

    Form for voting now just represents an IterationVote objected instead of the messy, nested Iteration crap.

--- app/models/iteration.rb
+++ app/models/iteration.rb
@@ -1,6 +1,4 @@
 class Iteration < ActiveRecord::Base
   belongs_to :turkee_task, :class_name => 'Turkee::TurkeeTask'
   has_many :iteration_votes
-
-  accepts_nested_attributes_for :iteration_votes
 end
