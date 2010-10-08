576ea7895f44ad36b2faf1ff4c98cddd8b15e0ea
Jim Jones <jjones@aantix.com>
2010-10-08 01:08:31 -0700

    Console dir was a mistake. Removing.

--- console/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '367ddbc3f86d6f90941e8de71a0402b58d1c82913d1947745f5793c4e757d2bd3d035bb02b1b9ce3e92b61112d67efe7f30bb95fb0b6cf513abc65411193d6ab';
