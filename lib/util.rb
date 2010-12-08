92b4a8be92db22c79aaf869b0a88439d18193c58
Jim Jones <jjones@aantix.com>
2010-12-07 20:35:31 -0800

    Added the port to the host_url function so that I can test locally.

--- lib/util.rb
+++ lib/util.rb
@@ -1,7 +1,7 @@
 class Util
   def self.host_url(url)
     referer     = URI::parse(url)
-    host        = "#{referer.scheme}://#{referer.host}"
+    host        = "#{referer.scheme}://#{referer.host}:#{referer.port}"
     host
   end
 end
